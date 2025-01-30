2937ece1acd7b94cdc87ca375f7e81d1009cf661
Gyeongmin Kim <gmkim716@gmail.com>
2025-01-30 13:38:20 +0900

    feat: react-query를 추가합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./widgets/ui/Header.tsx";
+import Footer from "./widgets/ui/Footer.tsx";
+import PostsManagerPage from "./pages/PostsManagerPage.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </QueryClientProvider>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
