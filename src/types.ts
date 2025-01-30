8cb94f6acaf46140fd28cb1bbbc099821f3cfc45
Gyeongmin Kim <gmkim716@gmail.com>
2025-01-30 13:13:46 +0900

    fix: 타입을 설정한다

---
+++ src/types.ts
@@ -0,0 +1,53 @@
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  tags: string[];
+  reactions: { likes: number; dislikes: number };
+  views: number;
+  userId: number;
+  author: Author;
+}
+
+export interface Comment {
+  id: number;
+  user: User;
+  body: string;
+  likes: number;
+}
+
+export interface Tag {
+  url: string;
+  slug: string;
+}
+
+export interface User {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: string;
+  address: Address;
+  phone: string;
+  company: Company;
+  age: number;
+  image: string;
+}
+
+export interface Author {
+  image: string;
+  username: string;
+}
+
+export interface Company {
+  name: string;
+  title: string;
+}
+
+export interface Address {
+  address: string;
+  state: string;
+  city: string;
+}
